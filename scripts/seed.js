c13238fa594ba47266245f2764e34f6a91a830f6
uL1ma1226 <evanobrera@gmail.com>
2024-01-17 14:50:27 +0800

    added main function for seeding DB

--- scripts/seed.js
+++ scripts/seed.js
@@ -80,3 +80,19 @@ async function seedProducts(client) {
         throw error;
     }
 }
+
+async function main() {
+    const client = await db.connect();
+  
+    await seedUsers(client);
+    await seedProducts(client);
+  
+    await client.end();
+  }
+  
+  main().catch((err) => {
+    console.error(
+      'An error occurred while attempting to seed the database:',
+      err,
+    );
+  });
